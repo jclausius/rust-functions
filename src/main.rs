1b5910ec0a8d14496e8be4b8cde1a215331dab5a
jclausius <jclausius@gmail.com>
2020-06-20 22:39:28 -0500

    assignment of a block

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ fn main() {
     println!("Hello, world!");
 
     another_function(243, -3);
+
+    yet_another_func();
+
+   let x = 6;
+
+   let y = {
+      let x = 3;
+      x+1
+   };
+
+   println!("value of y is {}", y);
 }
 
 fn another_function(x: i32, y: i32) -> i32
@@ -21,4 +32,22 @@ fn another_function(x: i32, y: i32) -> i32
 
   // function ends in an expression
   return x * y;
+}
+
+fn yet_another_func()
+{
+  // Calling a function is an expression. 
+  // Calling a macro is an expression. 
+  // The block used to create new scopes, {}, is an expression
+
+   let x = 22;
+
+   let y = {
+      println!("inside a scope block");
+      let x = 9;
+       x - 4
+   };
+
+   println!("The value of y was assigned from the last epxression in the scope of {}", y);
+
 }
